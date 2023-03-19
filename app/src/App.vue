d2e436d0fc7dd150e6da361b01dc2f60108c31f5
tech75rus <tech75rus@mail.ru>
2023-03-19 17:58:07 +0900

    added menu and font

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="wrapper">
-    <div class="menu"></div>
+    <div class="menu">
+      <div class="center">
+        <div class="logo">
+          <p>Marina design</p>
+        </div>
+        <div class="user">
+          <p>User</p>
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -14,17 +23,52 @@ body {
   background: #f6f1ff;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+//  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter-Regular;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 .wrapper {
-  margin: 0 auto;
+  margin: 100px auto 0 auto;
   max-width: 1198px;
   min-width: 280px;
 }
+.menu {
+  width: 100%;
+  height: 70px;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  box-shadow: 2px 0 12px -4px #000000;
+
+  .center {
+    margin: 0 auto;
+    max-width: 1198px;
+    width: 95%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo{
+      position: relative;
+    }
+
+  }
+}
+@font-face {
+  font-family: "Inter-Regular";
+  font-weight: 400;
+  font-style: normal;
+  font-display: auto;
+  src: local('Inter'), url("./assets/font-family/Inter-Regular.ttf") format("truetype");
+}
+
+
+
 //nav {
 //  padding: 30px;
 //
