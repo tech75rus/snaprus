0ca9cfbcf563ea0bc78e3d12623dd9ffb3f1187d
tech75rus <tech75rus@mail.ru>
2023-05-16 18:31:18 +0900

    наброски для записи изображения

--- api/src/Entity/Work.php
+++ api/src/Entity/Work.php
@@ -135,4 +135,94 @@ class Work
 
         return $this;
     }
+
+    public function setImages($imageFiles): self
+    {
+        $arrayImage = [];
+        if (empty($imageFiles)) {
+            return $this;
+        }
+        if (!file_exists($this->dirImage)) { // существует ли директория
+            mkdir($this->dirImage, 0777, true);
+        }
+
+        $uniqProduct = uniqid('/'); // уникальное имя папки продукта
+        $arrayImage[$uniqProduct] = []; // сохраняем уникальное имя пакпки в массиве
+
+        mkdir($this->dirImage . $uniqProduct); // создаем папку продукта
+
+        foreach ($imageFiles as $img) {
+            $uniqImage = uniqid('/'); // уникальное имя папки изображение
+            $arrayImage[$uniqProduct][$uniqImage] = []; // сохраняем его в массиве
+
+            $text = strstr($img, ',', false); // обрезаем не нужное
+            $base64 = substr($text, 1); // еще немного обрезаем
+            $bin = base64_decode($base64); // декодируем иизображение
+            if (strlen($bin) > 7400000) { // проверям на размер изображения
+                continue;
+            }
+
+            $result = imagecreatefromstring($bin); // создаем изображение для GD
+
+            $uniqOrigin = uniqid('/origin-') . '.webp';
+            mkdir($this->dirImage . $uniqProduct . $uniqImage); // создаем папку для изображения
+            $fullPath = $this->dirImage . $uniqProduct . $uniqImage . $uniqOrigin;
+            imagewebp($result, $fullPath);
+            $arrayImage[$uniqProduct][$uniqImage][] = $uniqOrigin;
+
+            $gd = imagecreatefromwebp($fullPath);
+
+            $uniq600 = uniqid('/600x600-') . '.webp';
+            $result = imagescale($gd, 600, 600);
+            $fullPath = $this->dirImage . $uniqProduct . $uniqImage . $uniq600;
+            imagewebp($result, $fullPath); // сохраняем изображение
+            $arrayImage[$uniqProduct][$uniqImage][] = $uniq600;
+
+            $uniq300 = uniqid('/300x300-') . '.webp';
+            $result = imagescale($gd, 300, 300);
+            $fullPath = $this->dirImage . $uniqProduct . $uniqImage . $uniq300;
+            imagewebp($result, $fullPath);
+            $arrayImage[$uniqProduct][$uniqImage][] = $uniq300;
+
+            $uniq150 = uniqid('/150x150-') . '.webp';
+            $result = imagescale($gd, 150, 150);
+            $fullPath = $this->dirImage . $uniqProduct . $uniqImage . $uniq150;
+            imagewebp($result, $fullPath);
+            $arrayImage[$uniqProduct][$uniqImage][] = $uniq150;
+
+        }
+
+        $this->image =  $arrayImage;
+
+        return $this;
+    }
+
+    public function deleteImages(array $images = []): bool
+    {
+        if (empty($images)) {
+            return false;
+        }
+        $productPath = '';
+        $imagePath = [];
+        $fullPath = [];
+        foreach ($images as $product => $img) {
+            $productPath = $this->dirImage . $product;
+            foreach ($img as $key => $items) {
+                $imagePath[] = $this->dirImage . $product . $key;
+                foreach ($items as $item) {
+                    $text = $this->dirImage . $product . $key;
+                    $text .= $item;
+                    $fullPath[] = $text;
+                }
+            }
+        }
+        foreach ($fullPath as $path) {
+            unlink($path);
+        }
+        foreach ($imagePath as $item) {
+            rmdir($item);
+        }
+        rmdir($productPath);
+        return true;
+    }
 }
