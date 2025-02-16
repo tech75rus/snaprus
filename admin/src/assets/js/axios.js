dc5b0543e43ccf03c4da116e0ed04ec44018ae35
tech75rus <tech75rus@mail.ru>
2025-02-16 16:46:56 +0900

    axios admin добавил interceptors для обновления заголовка token

--- admin/src/assets/js/axios.js
+++ admin/src/assets/js/axios.js
@@ -6,9 +6,23 @@ localStorage.setItem('app', 1);
 const apiClient = axios.create({
   baseURL: process.env.VUE_APP_URL,
   headers: {
-    'app': localStorage.getItem('app'),
-    'token': localStorage.getItem('token')
+    'app': localStorage.getItem('app')
   },
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['token'] = token;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
 
 export default apiClient;
