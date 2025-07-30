5570da34d5722c6c9901d8f1446fbdf859863425
tech75rus <tech75rus@mail.ru>
2025-07-30 12:28:50 +0900

    скрыл меню с пользователями

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,7 +6,7 @@
           <router-link to="/" style="text-decoration: none; color: #2c3e50;">FotoStudio</router-link>
         </div>
 
-        <div class="user">
+        <!-- <div class="user">
           <div class="icon" @click="toggleUserBlock()">
             <p>User</p>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="black" xmlns="http://www.w3.org/2000/svg">
@@ -17,7 +17,7 @@
             <span>Вход</span>
             <span>Регистрация</span>
           </div>
-        </div>
+        </div> -->
 
       </div>
     </div>
